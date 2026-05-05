2f746e337e877fc58e8cd58e0f76616b6bf48d83
marceltreptow <olafschnedet@yahoo.de>
2026-05-05 22:17:59 +0200

    add get all products page and logic

--- storefront/playground/urls.py
+++ storefront/playground/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.say_hello),
+    path('allProducts/', views.get_products)
 ]
--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
-from django.db.models import Q
+from django.db.models import Q, F
 
 # Create your views here.
 def say_hello(request):
-    products = Product.objects.all()
+    # Sort output
+    products = Product.objects.all().order_by('price', '-title')
+    # productsingle = Product.objects.all().order_by('price', '-title')[0]
+    # products = Product.objects.all().order_by('price', '-title').reverse()
 
+    # Every output
     for product in products:
        print(product)
 
+    # Query output depending on values in fields
     producty = Product.objects.filter(price__gt=6, pk=1)
     for p in producty:
         print(p)
         
+    # Query output with Complex Query operator
     producty = Product.objects.filter(Q(price__gt=6) | Q(pk=1))
     for p in producty:
         print(p)
 
+    # Check if product exists and print
     if (Product.objects.filter(pk=1).exists()):
         productx = Product.objects.filter(pk=1)
         print(productx)
-        
+
+    # Limit query values to save costs
+    # limitedproductsvalues = Product.objects.all().values('price')
+
+    # interjoin
+    limitedproductsvalues = Product.objects.all().values('promotion__discount')
+
     
-    return render(request, 'hello.html', {'name': 'Marcel', 'products': producty})
+    return render(request, 'hello.html', {'name': 'Marcel', 'products': products, 'limitedproductsvalues': limitedproductsvalues})
+
+def get_products(request):
+    product = Product.objects.all()
+    return render(request, 'test.html', {'products': product})
